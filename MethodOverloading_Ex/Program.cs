198132807c0491c33b32141df8e5a794d064803a
Bhandari007 <bmuskan007@gmail.com>
2022-04-19 19:46:35 +0545

    Added Method Overloading Example

---
+++ MethodOverloading_Ex/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MethodOverloading_Ex
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Area of rectangle:{Shape.Area(12, 13)}");
+            Console.WriteLine($"Area of square :{Shape.Area(12)}");
+        }
+    }
+}
---
+++ MethodOverloading_Ex/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading_Ex
+{
+    public static class Shape
+    {
+        // Area of Rectangle
+        public static int Area(int l,int b)
+        {
+            return l * b;
+        }
+        // Area of Square
+        public static int Area(int l)
+        {
+            return l * l;
+        }
+    }
+}
