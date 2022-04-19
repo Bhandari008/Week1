e8292c3d62d82b61b2f7dc6d30a513668fbbde0e
Bhandari007 <bmuskan007@gmail.com>
2022-04-19 16:22:22 +0545

    Added Interface Example (Multiple Inheritance)

---
+++ Interface_Ex/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Ex
+{
+    // Interface 1 "Animal"
+    public interface Animal
+    {
+        public void displayA() { }
+    }
+    // Interface 2 "Domestic"
+    public interface Domestic
+    {
+        public void displayD() { }
+    }
+
+    // class "Dog" from interface1 and interface2
+    public class Dog:Animal,Domestic
+    {
+        public void displayA()
+        {
+            Console.WriteLine("Animal Interface Method");
+        }
+        public void displayD()
+        {
+            Console.WriteLine("Domestic Interface Method");
+        }
+    }
+}
---
+++ Interface_Ex/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Interface_Ex
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.displayA();
+            d.displayD();
+        }
+    }
+}
