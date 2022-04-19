a0c6837f87b26501fba8b4a09b75a742db56c794
Bhandari007 <bmuskan007@gmail.com>
2022-04-19 19:37:53 +0545

    Added Static Class Example

---
+++ Static_Ex/Area.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Ex
+{
+    public static class Area
+    {
+        public static int Rectangle(int l,int b)
+        {
+            return l * b;
+        }
+        public static int Square(int l)
+        {
+            return l * l;
+        }
+    }
+}
---
+++ Static_Ex/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Static_Ex
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Area of rectangle is :{Area.Rectangle(12, 13)}");
+            Console.WriteLine($"Area of square is :{Area.Square(12)}");
+        }
+    }
+}
