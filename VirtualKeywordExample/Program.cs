d5d95a9f1ca4164c7d1046a6d82e6d2f96904ef7
Bhandari007 <bmuskan007@gmail.com>
2022-04-21 16:53:40 +0545

    Added virtual keyword example

---
+++ VirtualKeywordExample/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace VirtualKeywordExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parallelogram s = new Parallelogram();
+            s.Area(12, 13);
+        }
+    }
+}
---
+++ VirtualKeywordExample/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualKeywordExample
+{
+    public class Shape
+    {
+        public virtual void  Area(int l,int b)
+        {
+            Console.WriteLine($"Area of Rectangle:{l * b}");
+        }
+    }
+    public class Parallelogram:Shape
+    {
+        public override void Area(int l,int b)
+        {
+            Console.WriteLine($"Area of Parallelogram:{(l * b)/2}");
+        }
+    }
+}
