a01aa6d16f52c49e1a4ebcaf9aa53d17d20a61d1
Bhandari007 <bmuskan007@gmail.com>
2022-04-21 17:02:14 +0545

    Added Indexers Example

---
+++ IndexersExample/Collection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersExample
+{
+    public class Collection
+    {
+        private string[] _collection = new string[10];
+        // indexer property
+        public string this[int idx]
+        {
+            get
+            {
+                if (idx > 9)
+                {
+                    Console.WriteLine("Index out of range");
+                    return "";
+                }
+                return _collection[idx];
+            }
+            set
+            {
+                if(idx>9)
+                {
+                    Console.WriteLine("Index out of range");
+                }
+                _collection[idx] = value;
+            }
+        }
+    }
+}
---
+++ IndexersExample/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace IndexersExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Collection col = new Collection();
+            col[0] = "John Doe";
+            col[9] = "Hari Devkota";
+            Console.WriteLine($"{col[0]}\t{col[9]}");
+            //col[10] = "Muskan Bhandari"; Error 
+        }
+    }
+}
